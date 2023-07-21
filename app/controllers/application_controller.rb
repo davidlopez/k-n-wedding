b21c19d304808b8b9e40c0da74124d1be530a214
David Lopez <david@davidclopez.me>
2023-07-21 17:39:08 -0600

    Adds invites

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+    def index
+        render html: "Hello, world!"
+        true
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,347 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+invite1 = Invite.create(code: "FDBD91")
+invite1.rsvps << Rsvp.create(name: "Michelle Lopez")
+invite1.rsvps << Rsvp.create(name: "Chris Lopez")
+
+invite2 = Invite.create(code: "D2202C")
+invite2.rsvps << Rsvp.create(name: "David Lopez")
+invite2.rsvps << Rsvp.create(name: "Madison Lopez")
+
+invite3 = Invite.create(code: "230AB9")
+invite3.rsvps << Rsvp.create(name: "Brian Martinez")
+invite3.rsvps << Rsvp.create(name: "Renee Martinez")
+invite3.rsvps << Rsvp.create(name: "Grace Martinez")
+
+invite4 = Invite.create(code: "CC7816")
+invite4.rsvps << Rsvp.create(name: "Dale Lopez")
+invite4.rsvps << Rsvp.create(name: "Alice Lopez")
+
+invite5 = Invite.create(code: "AEDAC9")
+invite5.rsvps << Rsvp.create(name: "Steve Lopez")
+invite5.rsvps << Rsvp.create(name: "Juanita Haynie")
+
+invite6 = Invite.create(code: "9C2754")
+invite6.rsvps << Rsvp.create(name: "Janice Gonzales")
+invite6.rsvps << Rsvp.create(name: "Ernie Gonzales")
+invite6.rsvps << Rsvp.create(name: "Taylor Gonzales")
+
+invite7 = Invite.create(code: "5499F2")
+invite7.rsvps << Rsvp.create(name: "Brittany Lopez")
+
+invite8 = Invite.create(code: "7997BC")
+invite8.rsvps << Rsvp.create(name: "Sabrina Lopez")
+invite8.rsvps << Rsvp.create(name: "Devin Minardi")
+
+invite9 = Invite.create(code: "18F4A7")
+invite9.rsvps << Rsvp.create(name: "Mike Lopez")
+invite9.rsvps << Rsvp.create(name: "Susie Lopez")
+invite9.rsvps << Rsvp.create(name: "Mikey Lopez")
+
+invite10 = Invite.create(code: "45DD34")
+invite10.rsvps << Rsvp.create(name: "Kirk Siekman")
+invite10.rsvps << Rsvp.create(name: "Jan Siekman")
+
+invite11 = Invite.create(code: "B33B6D")
+invite11.rsvps << Rsvp.create(name: "Roy Lesueur")
+
+invite12 = Invite.create(code: "77B25C")
+invite12.rsvps << Rsvp.create(name: "Michael Siekman")
+invite12.rsvps << Rsvp.create(name: "Katie Savage")
+
+invite13 = Invite.create(code: "7C5791")
+invite13.rsvps << Rsvp.create(name: "Andrew Siekman")
+invite13.rsvps << Rsvp.create(name: "Penny Siekman")
+invite13.rsvps << Rsvp.create(name: "Morgan Siekman")
+
+invite14 = Invite.create(code: "76EF10")
+invite14.rsvps << Rsvp.create(name: "Kyle Kuschel")
+invite14.rsvps << Rsvp.create(name: "Kari Kuschel")
+
+invite15 = Invite.create(code: "4505C6")
+invite15.rsvps << Rsvp.create(name: "Brant Epperhart")
+invite15.rsvps << Rsvp.create(name: "Abi Epperhart")
+
+invite16 = Invite.create(code: "71FA7E")
+invite16.rsvps << Rsvp.create(name: "Eli Johnson")
+
+invite17 = Invite.create(code: "C6D164")
+invite17.rsvps << Rsvp.create(name: "Jason George")
+invite17.rsvps << Rsvp.create(name: "Yasmine")
+invite17.rsvps << Rsvp.create(name: "Yandel")
+invite17.rsvps << Rsvp.create(name: "Jessica")
+
+invite18 = Invite.create(code: "C57EFB")
+invite18.rsvps << Rsvp.create(name: "Amber Tarrant")
+
+invite19 = Invite.create(code: "A098D9")
+invite19.rsvps << Rsvp.create(name: "Katie Donaldson")
+
+invite20 = Invite.create(code: "FF5E8E")
+invite20.rsvps << Rsvp.create(name: "Deb Donaldson")
+invite20.rsvps << Rsvp.create(name: "Rick Donaldson")
+
+invite21 = Invite.create(code: "E06A33")
+invite21.rsvps << Rsvp.create(name: "Danny Martinez")
+invite21.rsvps << Rsvp.create(name: "Megan Martinez")
+
+invite22 = Invite.create(code: "160B79")
+invite22.rsvps << Rsvp.create(name: "Anthony Trujillo")
+invite22.rsvps << Rsvp.create(name: "Dawn Trujillo")
+
+invite23 = Invite.create(code: "E093C9")
+invite23.rsvps << Rsvp.create(name: "Jill Michael")
+invite23.rsvps << Rsvp.create(name: "Todd Michael")
+invite23.rsvps << Rsvp.create(name: "Tanner Michael")
+invite23.rsvps << Rsvp.create(name: "Pierson Michael")
+invite23.rsvps << Rsvp.create(name: "Tyler Michael")
+invite23.rsvps << Rsvp.create(name: "Molly OBuckley")
+
+invite24 = Invite.create(code: "DA037E")
+invite24.rsvps << Rsvp.create(name: "David Dimas")
+
+invite25 = Invite.create(code: "A1AC3D")
+invite25.rsvps << Rsvp.create(name: "Teresa Bramel")
+invite25.rsvps << Rsvp.create(name: "Robert Bramel")
+invite25.rsvps << Rsvp.create(name: "Jasmine Bramel")
+invite25.rsvps << Rsvp.create(name: "Beau Bramel")
+
+invite26 = Invite.create(code: "66ED46")
+invite26.rsvps << Rsvp.create(name: "Rick Munger")
+invite26.rsvps << Rsvp.create(name: "Cindy Munger")
+
+invite27 = Invite.create(code: "4CDBDF")
+invite27.rsvps << Rsvp.create(name: "Marcia Dyrland")
+
+invite28 = Invite.create(code: "F828D4")
+invite28.rsvps << Rsvp.create(name: "Carlos Catalan")
+invite28.rsvps << Rsvp.create(name: "Jessie Catalan")
+
+invite29 = Invite.create(code: "EC71EC")
+invite29.rsvps << Rsvp.create(name: "Barbara Du Kett")
+invite29.rsvps << Rsvp.create(name: "Sterling Du Kett")
+invite29.rsvps << Rsvp.create(name: "Parker Du Kett")
+invite29.rsvps << Rsvp.create(name: "Marshal Du Kett")
+invite29.rsvps << Rsvp.create(name: "Lacy Miles")
+
+invite30 = Invite.create(code: "A23D6A")
+invite30.rsvps << Rsvp.create(name: "Bev White")
+invite30.rsvps << Rsvp.create(name: "Floyd White")
+
+invite31 = Invite.create(code: "0B0420")
+invite31.rsvps << Rsvp.create(name: "Jess Potter")
+invite31.rsvps << Rsvp.create(name: "Jim Nix")
+
+invite32 = Invite.create(code: "52EB63")
+invite32.rsvps << Rsvp.create(name: "Mike Shotwell")
+invite32.rsvps << Rsvp.create(name: "Lynn Shotwell")
+
+invite33 = Invite.create(code: "852C23")
+invite33.rsvps << Rsvp.create(name: "Dave Olds")
+invite33.rsvps << Rsvp.create(name: "Darla Olds")
+
+invite34 = Invite.create(code: "68BE76")
+invite34.rsvps << Rsvp.create(name: "Eva Ortiz")
+
+invite35 = Invite.create(code: "7D0E32")
+invite35.rsvps << Rsvp.create(name: "Laura Sisneros")
+invite35.rsvps << Rsvp.create(name: "John Sisneros")
+
+invite36 = Invite.create(code: "8B32D9")
+invite36.rsvps << Rsvp.create(name: "James Archuleta")
+invite36.rsvps << Rsvp.create(name: "Rosanna Archuleta")
+
+invite37 = Invite.create(code: "0FC551")
+invite37.rsvps << Rsvp.create(name: "Margo Ankele")
+invite37.rsvps << Rsvp.create(name: "Jeff Ankele")
+
+invite38 = Invite.create(code: "0E4A16")
+invite38.rsvps << Rsvp.create(name: "Melva Jane Sainz")
+invite38.rsvps << Rsvp.create(name: "Paul Sainz")
+
+invite39 = Invite.create(code: "91F09F")
+invite39.rsvps << Rsvp.create(name: "Phil Sainz")
+
+invite40 = Invite.create(code: "FF1AB9")
+invite40.rsvps << Rsvp.create(name: "Esther Martinez")
+invite40.rsvps << Rsvp.create(name: "Jerry Martinez")
+
+invite41 = Invite.create(code: "519B5E")
+invite41.rsvps << Rsvp.create(name: "Donna")
+invite41.rsvps << Rsvp.create(name: "Dave")
+
+invite42 = Invite.create(code: "7E400A")
+invite42.rsvps << Rsvp.create(name: "Carolyn")
+invite42.rsvps << Rsvp.create(name: "Bruce")
+
+invite43 = Invite.create(code: "C43ACC")
+invite43.rsvps << Rsvp.create(name: "Krista Siekman")
+invite43.rsvps << Rsvp.create(name: "Arent")
+invite43.rsvps << Rsvp.create(name: "Logan")
+invite43.rsvps << Rsvp.create(name: "Charlotte")
+
+invite44 = Invite.create(code: "642361")
+invite44.rsvps << Rsvp.create(name: "Brittany Rennecker")
+
+invite45 = Invite.create(code: "FAFEFF")
+invite45.rsvps << Rsvp.create(name: "Nicole Siekman")
+
+invite46 = Invite.create(code: "EE737A")
+invite46.rsvps << Rsvp.create(name: "Becky Edgar")
+invite46.rsvps << Rsvp.create(name: "Mark Edgar")
+
+invite47 = Invite.create(code: "147921")
+invite47.rsvps << Rsvp.create(name: "Allen Siekman")
+invite47.rsvps << Rsvp.create(name: "Sue Siekman")
+
+invite48 = Invite.create(code: "B3440C")
+invite48.rsvps << Rsvp.create(name: "Jean Linza")
+invite48.rsvps << Rsvp.create(name: "Lanny Linza")
+invite48.rsvps << Rsvp.create(name: "Lynn Linza")
+
+invite49 = Invite.create(code: "5D0B18")
+invite49.rsvps << Rsvp.create(name: "Marcia Watkins")
+invite49.rsvps << Rsvp.create(name: "Shane Watkins")
+
+invite50 = Invite.create(code: "30CDF2")
+invite50.rsvps << Rsvp.create(name: "Julie Roghair")
+invite50.rsvps << Rsvp.create(name: "Mel Roghair")
+
+invite51 = Invite.create(code: "56AD4E")
+invite51.rsvps << Rsvp.create(name: "Josh Worrell")
+
+invite52 = Invite.create(code: "93F7DE")
+invite52.rsvps << Rsvp.create(name: "Joanne Roxson")
+invite52.rsvps << Rsvp.create(name: "Mario Roxson")
+invite52.rsvps << Rsvp.create(name: "Keely Hamilton")
+invite52.rsvps << Rsvp.create(name: "Meme Robinson")
+invite52.rsvps << Rsvp.create(name: "Kenzie Robinson")
+
+invite53 = Invite.create(code: "35A64D")
+invite53.rsvps << Rsvp.create(name: "Ryan Hamilton")
+invite53.rsvps << Rsvp.create(name: "Monica Hamilton")
+invite53.rsvps << Rsvp.create(name: "Cassidy Hamilton")
+invite53.rsvps << Rsvp.create(name: "Waylon Hamilton")
+
+invite54 = Invite.create(code: "C8E69D")
+invite54.rsvps << Rsvp.create(name: "Richard Robinson")
+invite54.rsvps << Rsvp.create(name: "Jessica Romero?????")
+
+invite55 = Invite.create(code: "F3015B")
+invite55.rsvps << Rsvp.create(name: "Doug Robinson")
+invite55.rsvps << Rsvp.create(name: "Missy Robinson")
+invite55.rsvps << Rsvp.create(name: "Cammy Robinson")
+invite55.rsvps << Rsvp.create(name: "Gunner Robinson")
+
+invite56 = Invite.create(code: "F4023C")
+invite56.rsvps << Rsvp.create(name: "Bobby Hamilton")
+invite56.rsvps << Rsvp.create(name: "Kathy Hamilton")
+
+invite57 = Invite.create(code: "7E6A4E")
+invite57.rsvps << Rsvp.create(name: "Tanner Luedtke")
+invite57.rsvps << Rsvp.create(name: "Tyler Luedtke")
+invite57.rsvps << Rsvp.create(name: "Teigan Luedtke")
+invite57.rsvps << Rsvp.create(name: "Jeff Luedtke")
+invite57.rsvps << Rsvp.create(name: "Hawley Luedtke")
+
+invite58 = Invite.create(code: "FE4B5B")
+invite58.rsvps << Rsvp.create(name: "Steven Sommer")
+invite58.rsvps << Rsvp.create(name: "Lisa Bailey")
+
+invite59 = Invite.create(code: "940C3E")
+invite59.rsvps << Rsvp.create(name: "Robert Wilder")
+invite59.rsvps << Rsvp.create(name: "Vickie Wilder")
+
+invite60 = Invite.create(code: "3C5C09")
+invite60.rsvps << Rsvp.create(name: "Dorthy Phillips")
+invite60.rsvps << Rsvp.create(name: "Matt Phillips")
+invite60.rsvps << Rsvp.create(name: "Winston")
+invite60.rsvps << Rsvp.create(name: "Brantley")
+invite60.rsvps << Rsvp.create(name: "Warren")
+invite60.rsvps << Rsvp.create(name: "Walker")
+invite60.rsvps << Rsvp.create(name: "Riley")
+
+invite61 = Invite.create(code: "828883")
+invite61.rsvps << Rsvp.create(name: "DJ Robinson")
+invite61.rsvps << Rsvp.create(name: "Ashley Robinson")
+invite61.rsvps << Rsvp.create(name: "Maverick Robinson")
+invite61.rsvps << Rsvp.create(name: "Carson Robinson")
+
+invite62 = Invite.create(code: "B48CE1")
+invite62.rsvps << Rsvp.create(name: "Buck Robinson")
+invite62.rsvps << Rsvp.create(name: "Judy Robinson")
+
+invite63 = Invite.create(code: "B70BA3")
+invite63.rsvps << Rsvp.create(name: "Stacey Hamilton")
+invite63.rsvps << Rsvp.create(name: "Caleb Hansen")
+invite63.rsvps << Rsvp.create(name: "Corbin Hansen")
+
+invite64 = Invite.create(code: "3D3433")
+invite64.rsvps << Rsvp.create(name: "J.T Johnson")
+invite64.rsvps << Rsvp.create(name: "Leslie Aspen")
+
+invite65 = Invite.create(code: "A751B6")
+invite65.rsvps << Rsvp.create(name: "Rick Robinson")
+invite65.rsvps << Rsvp.create(name: "Ashley Robinson")
+invite65.rsvps << Rsvp.create(name: "Jagger Robinson")
+invite65.rsvps << Rsvp.create(name: "Mollie Robinson")
+invite65.rsvps << Rsvp.create(name: "Katie Robinson")
+
+invite66 = Invite.create(code: "A6FE6B")
+invite66.rsvps << Rsvp.create(name: "Issac Branson")
+invite66.rsvps << Rsvp.create(name: "Jay Branson")
+invite66.rsvps << Rsvp.create(name: "Amanda Branson")
+invite66.rsvps << Rsvp.create(name: "Olivia Branson")
+invite66.rsvps << Rsvp.create(name: "Silas Branson")
+
+invite67 = Invite.create(code: "FAF17B")
+invite67.rsvps << Rsvp.create(name: "Donnie Robinson")
+invite67.rsvps << Rsvp.create(name: "Rochelle Robinson")
+
+invite68 = Invite.create(code: "197A94")
+invite68.rsvps << Rsvp.create(name: "John Robinson")
+invite68.rsvps << Rsvp.create(name: "Kayla Robinson")
+invite68.rsvps << Rsvp.create(name: "Maddie Robinson")
+invite68.rsvps << Rsvp.create(name: "Presley Robinson")
+
+invite69 = Invite.create(code: "FDDE10")
+invite69.rsvps << Rsvp.create(name: "Kyle Wilkes")
+
+invite70 = Invite.create(code: "F9589F")
+invite70.rsvps << Rsvp.create(name: "Ben Owens")
+
+invite71 = Invite.create(code: "0519C3")
+invite71.rsvps << Rsvp.create(name: "Elaina Farner")
+invite71.rsvps << Rsvp.create(name: "Aiden Farner")
+
+invite72 = Invite.create(code: "AD988F")
+invite72.rsvps << Rsvp.create(name: "Becky McDonald")
+
+invite73 = Invite.create(code: "168AC4")
+invite73.rsvps << Rsvp.create(name: "Derrick King")
+invite73.rsvps << Rsvp.create(name: "Sheri King")
+
+invite74 = Invite.create(code: "1A219F")
+invite74.rsvps << Rsvp.create(name: "Jordan Rock")
+
+invite75 = Invite.create(code: "49398E")
+invite75.rsvps << Rsvp.create(name: "Sheryl Prince")
+
+invite76 = Invite.create(code: "CA56E3")
+invite76.rsvps << Rsvp.create(name: "Jake Hibben")
+invite76.rsvps << Rsvp.create(name: "Savannah Hibben")
+
+invite77 = Invite.create(code: "D6731F")
+invite77.rsvps << Rsvp.create(name: "Addi Bong")
+
+invite78 = Invite.create(code: "1190A9")
+invite78.rsvps << Rsvp.create(name: "Marc Martinez")
+invite78.rsvps << Rsvp.create(name: "Julie Martinez")
+
+invite79 = Invite.create(code: "F39284")
+invite79.rsvps << Rsvp.create(name: "Rebecca Bateman")
+
+invite80 = Invite.create(code: "E4F0B9")
+invite80.rsvps << Rsvp.create(name: "Gavin Brown")
+invite80.rsvps << Rsvp.create(name: "Miranda Brown")
+invite80.rsvps << Rsvp.create(name: "Amery Brown")
+invite80.rsvps << Rsvp.create(name: "Maddie Roberts")
+invite80.rsvps << Rsvp.create(name: "Jay Roberts")
